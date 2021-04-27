480316a747a598354e73b9195374dd08db2a2754
Christine Contreras <christineec23@yahoo.com>
2021-04-26 20:20:04 -0500

    completed pod 1 tasks

--- javascript-dom/README.md
+++ javascript-dom/README.md
@@ -1,26 +1,60 @@
 # Intro to JavaScript & DOM Manipulation
 
 ### 1. Write a function in at least two different ways.
+function hello() {
+    console.log('im a function');
+}
+
+const hello = function() {
+    console.log('im a function');
+}
+
+const hello = () => console.log('im a function');
+
 
 ### 2. What are the two ways of creating an object? Show below.
+const object = {};
+
+const object = new Object();
 
 ### 3. What does it mean to destructively change an array?
+you change the original array. Doesn't return a new array
+
 
 ### 4. Name a few destructive array methods and what they do.
+.pop() : deletes from the end of the array
+.push() : adds to end of the array
+.shift() : delete from the beginning of the array
+.unshift() : add to beginning of the array
+
 
 ### 5. What does `.map` do? what does `.map` return?
+makes changes to array and creates a new array from the values. 
+
 
 ### 6. What is the difference between `.map` and `.forEach`?
+map has a return array and forEach doesn't return anything
+
 
 ### 7. What is a node?
+an object provided by the dom. like an element with properties 
 
 ### 8. What is the virtual DOM?
+representation of the source HTML
+
 
 ### 9. What does `.querySelector` return?
+the first selection of an element matching the querySelector. Returns the node
+
 
 ### 10. What does `.getElementsByClassName` return?
+return all nodes with the class name you are searching for. Node list
+
 
 ### 11. What is the difference between `.innerHTML` and `.textContent`?
+.innerHTML grabs all inner html elements and the string
+.textContent only grabs the string within the designated area.
+
 
 ### Continue in `index.js` . Open the html file by typing `open index.html` in your terminal after you `cd` into the `javascript-dom` folder. Note that `index.js` is not connected to your html file. Connect them and begin.
 
--- javascript-dom/index.js
+++ javascript-dom/index.js
@@ -1,8 +1,47 @@
+console.log('hello world!');
+
 // change the header content to say "Images"
+const header = document.querySelector('h1');
+header.textContent = "Images";
+
+
+
 
 // find at least 5 images and display them programmatically in image container
+const imgContainer = document.getElementById('image-container');
+
+const images = ['https://images.unsplash.com/photo-1617335875629-51c0f3a99607?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1267&q=80', 'https://images.unsplash.com/photo-1580141807143-f515f69099ee?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://images.unsplash.com/photo-1617184003170-4c9fc60cdb75?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://images.unsplash.com/photo-1616095530025-96ddb5c7d5f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80', 'https://images.unsplash.com/photo-1616095325915-17d508e0e4e0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'];
+
+images.forEach( img => {
+    const imgTag = document.createElement('img');
+    imgTag.src = img;
+
+    imgContainer.appendChild(imgTag);
+});
+
 
 // BONUS: add a description for each image
+const imageObjects = [
+    {
+        source: 'https://images.unsplash.com/photo-1619369537880-8206b88573d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
 
+        description: 'description #1'
+    },
+    {
+        source: 'https://images.unsplash.com/photo-1615918884033-1b43ba5b1817?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80',
+
+        description: 'description #2'
+    }
+]
+
+imageObjects.forEach( img => {
+    const imgTag = document.createElement('img');
+    const imgDescription = document.createElement('p');
+    imgTag.src = img.source;
+    imgDescription.textContent = img.description;
+
+    imgContainer.appendChild(imgTag);
+    imgContainer.appendChild(imgDescription);
+});
 
 
